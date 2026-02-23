1b531d68b1825cd2c6a3f049121aebe9acc8c6e9
Brain-Super-RY <rizkyyogasalasa@gmail.com>
2026-02-23 18:08:00 +0700

    Update Data

---
+++ src/tugas/praktikum/pkg02/BingoBeraksi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.praktikum.pkg02;
+
+/**
+ *
+ * @author ACER NITRO
+ */
+public class BingoBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek Bingo - konstruktor akan dijalankan otomatis
+        Bingo bingo = new Bingo();
+    }
+}
---
+++ src/tugas/praktikum/pkg02/MatematikaBeraksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.praktikum.pkg02;
+
+/**
+ *
+ * @author ACER NITRO
+ */
+public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek Matematika
+        Matematika mtk = new Matematika();
+        
+        // Memanggil method dan menampilkan hasil
+        System.out.println("Pertambahan : 20 + 10 = " + mtk.pertambahan(20, 10));
+        System.out.println("Pengurangan : 10 - 5 = " + mtk.pengurangan(10, 5));
+        System.out.println("Perkalian   : 10 * 3 = " + mtk.perkalian(10, 3));
+        System.out.println("Pembagian   : 21 / 2 = " + mtk.pembagian(21, 2));
+    }
+}
