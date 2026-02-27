27086f5cd6c63a2ff9551264f200deee8aad00b2
Brain-Super-RY <rizkyyogasalasa@gmail.com>
2026-02-27 12:01:02 +0700

    Update Data

---
+++ Praktikum-01/Mahasiswa.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.pertemuan.pkg01;
+
+/**
+ *
+ * @author ACER NITRO
+ */
+public class Mahasiswa {
+    // Method membaca()
+    public void membaca() {
+        System.out.println("Status: Mahasiswa sedang membaca");
+    }
+    
+    // Method nyontek()
+    public void nyontek() {
+        System.out.println("Status: Mahasiswa sedang nyontek");
+    }
+    
+    // Method modifikasi()
+    public void modifikasi() {
+        System.out.println("Status: Mahasiswa sedang memodifikasi");
+    }
+}
